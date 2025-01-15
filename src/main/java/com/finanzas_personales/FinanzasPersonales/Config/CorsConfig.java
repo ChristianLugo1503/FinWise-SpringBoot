8449e232dab8a45fce4923348afb1ff723e25413
Christian Lugo <christianlugo1503@gmail.com>
2025-01-14 20:02:04 -0700

    CORS reconfigurados

--- src/main/java/com/finanzas_personales/FinanzasPersonales/Config/CorsConfig.java
+++ src/main/java/com/finanzas_personales/FinanzasPersonales/Config/CorsConfig.java
@@ -1,6 +1,5 @@
 package com.finanzas_personales.FinanzasPersonales.Config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
@@ -15,12 +14,12 @@ public class CorsConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowCredentials(true);
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200", "https://ng-menu-dashboard.onrender.com"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST", "PUT", "DELETE"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.setAllowCredentials(true); // Esto permite enviar cookies o información de autenticación
+        configuration.setAllowedOrigins(Arrays.asList("*")); // Permite todos los orígenes
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // Métodos HTTP permitidos
+        configuration.setAllowedHeaders(Arrays.asList("*")); // Permite todos los encabezados
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
+        source.registerCorsConfiguration("/**", configuration); // Aplica CORS a todas las rutas
         return source;
     }
 }
